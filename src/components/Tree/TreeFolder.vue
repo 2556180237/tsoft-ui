bac885d99ea8e36879d0965f782c63bfc7b2f697
Dmitry Ostanin <dmitry@ostanin.email>
2022-04-05 18:27:13 +0300

    from child to parrent dist

--- src/components/Tree/TreeFolder.vue
+++ src/components/Tree/TreeFolder.vue
@@ -58,11 +58,11 @@ export default {
     getRegisters(name) {
       if(this.treeData.folders[name].settings.isLink) {
         this.$emit('getData', {
-          info: Object.keys(this.treeData.folders[name].folders)[0],
+          info: name,
         })
         alert('done');
         console.log(this.info);
-        console.log(Object.keys(this.treeData.folders[name].folders)[0]);
+        console.log(name);
       } else {
         alert(2);
       }
